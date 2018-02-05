0f91f637eedd7660efaa55686ec8ee0bf991304b
Malcolm Ramsay <malramsay64@gmail.com>
2018-02-05 16:33:41 +1100

    Include a test suite for the python components
    Since most of the logic is being performed in python it was easiest to
    use pytest which I am familiar with to perform the testing of the logic.
    This is going to pick up the majority of the bugs and forms a framework
    which I can use to develop the project by specifying functionality.

---
+++ rplugin/python3/nvim-project-log/tests/test_logDirectory.py
@@ -0,0 +1,53 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2018 Malcolm Ramsay <malramsay64@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+"""
+
+from pathlib import Path
+
+from logDirectory import LogDirectory
+
+CWD = Path(__file__).parent
+LOGDIR = CWD / 'test_log'
+
+def test_file_list():
+    log = LogDirectory(LOGDIR)
+    assert len(log.file_list) == 26
+    assert log.file_list == sorted(log.file_list, reverse=True)
+
+
+def test_file_index():
+    log = LogDirectory(LOGDIR)
+    for index, filename in enumerate(log):
+        print(index)
+        assert log.get_file_index(filename) == index
+
+
+def test_get_previous():
+    log = LogDirectory(LOGDIR)
+    prev_file = log.file_list[-1]
+    # Reversing iterates in chronological order
+    for filename in reversed(log):
+        assert log.get_previous(filename) == prev_file
+        prev_file = filename
+
+
+def test_get_next():
+    log = LogDirectory(LOGDIR)
+    next_file = log.get_entry()
+    for filename in log:
+        assert log.get_next(filename) == next_file
+        next_file = filename
+
+
+def test_create_in():
+    log = LogDirectory(LOGDIR)
+    expected_index = (LOGDIR / 'expected_index.md').read_text()
+    log.update_index()
+    assert log.get_index().read_text() == expected_index
